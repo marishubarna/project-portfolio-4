d25076fa64c866c1bce220eb0bdf181802d8cab8
Marianna Shuniaieva <shuniaevamarianna@gmail.com>
2024-05-01 12:45:39 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const bookButton = document.getElementById("book-button");
+const bookPopup = document.getElementById("book-popup");
+
+bookButton.addEventListener("click", () => {
+    bookPopup.classList.add})
